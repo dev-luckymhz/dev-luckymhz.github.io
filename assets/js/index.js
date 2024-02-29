15383fed84d8f954bd97e3096bf0b800dfccd9dd
AmbinintsoaZo <herizo@nomade.mg>
2024-02-29 11:53:53 +0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+
+    sections.forEach((i) => {
+        let top = window.scrollY;
+        let offset = i.offsetTop - 150;
+        let height = i.offsetHeight;
+        let id = i.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach((links) => {
+                links.classList.remove('active');
+                document.querySelector(`header nav a[href="#${id}"]`).classList.add('active');
+            });
+        }
+    });
+
+
+    let header = document.querySelector(".header");
+    header.classList.toggle( 'sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+new FroalaEditor('textarea');
